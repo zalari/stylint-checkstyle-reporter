fdf6b181f6c041bc8e734a30b842efdd7e89cff9
David Enke <david.enke@zalari.de>
2018-06-25 18:06:05 +0200

    added checkstyle generation

--- index.js
+++ index.js
@@ -1,31 +1,45 @@
-'use strict';
+const fs = require('fs');
+const path = require('path');
 
-var path     = require('path');
-var messages = [];
+const files = {};
+const getFileData = (file) => {
+  return files[file] || {
+    path: path.relative(process.cwd(), file),
+    messages: []
+  };
+};
 
+// TODO: move to (and publish) separate module
 module.exports = function (message, done) {
-    if (done === 'done') {
-        var result = [];
 
-        result.push({
-            filePath    : path.relative(process.cwd(), this.cache.file),
-            messages    : messages,
-            errorCount  : this.cache.errs.length,
-            warningCount: this.cache.warnings.length
-        });
+  if (done === 'done') {
+    const options = this.config.reporterOptions || {};
+    const reportPath = path.resolve(options.reportPath || 'stylint-checkstyle-report.xml');
+    const data = `<?xml version="1.0" encoding="utf-8"?>
+<checkstyle version="4.3">
+${Object.keys(files).map(file => `  <file name="${files[file].path}">
+${files[file].messages.map(message => `    <error line="${message.line}" column="${message.column}" severity="${message.severity}" message="${message.message}" source="${message.source}" />`).join("\r\n")}
+  </file>`).join("\r\n")}
+</checkstyle>`;
 
-        this.cache.msg = JSON.stringify(result);
+    fs.writeFileSync(reportPath, data);
 
-        return this.done();
-    }
+    console.log(`Stylint report written to ${reportPath}`);
 
-    messages.push({
-        severity: this.state.severity,
-        message : message,
-        rule    : this.cache.rule,
-        line    : this.cache.lineNo,
-        column  : this.cache.col
-    });
+    return this.done();
+  }
 
-    return '';
+  const fileData = getFileData(this.cache.file);
+  const { rule, lineNo, col } = this.cache;
+
+  fileData.messages.push({
+    message,
+    rule,
+    line: lineNo,
+    column: col,
+    severity: this.state.severity.toLowerCase(),
+    source: `failure.stylint.${rule}`
+  });
+
+  files[this.cache.file] = fileData;
 };
